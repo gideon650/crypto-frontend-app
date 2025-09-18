103d1b5c1edcce594ca8054b2afc0ffcd10a6528
gideon650 <nzubegideon@gmail.com>
2025-09-18 20:14:12 +0100

    fixes

--- src/components/TermsModal.js
+++ src/components/TermsModal.js
@@ -92,7 +92,7 @@ const TermsModal = ({ onAccept, show }) => {
 
             <h4>7. Referral Program</h4>
             <ul>
-              <li>Referral codes (e.g., REF00010) may be issued to users for promotional purposes.</li>
+              <li>Referral codes may be issued to users for promotional purposes.</li>
               <li>Referral benefits are subject to change or termination at SwapView's discretion.</li>
               <li>Fraudulent activities, such as creating fake accounts or circular transfers to exploit referral rewards, will result in account bans and loss of rewards.</li>
             </ul>
